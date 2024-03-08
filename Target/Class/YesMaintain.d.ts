f4a455e7b95dd34b89b8d5a9264646a2f4004505
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-08 13:14:33 +0200

    0.0.1

---
+++ Target/Class/YesMaintain.d.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+declare const _default: import("commander").Command;
+export default _default;
---
+++ Target/Variable/Cloudflare.js
@@ -0,0 +1,1 @@
+var e=new Set([{Path:"/workflows/",Name:"Cloudflare.yml",File:async()=>new Set([(await(await import("fs/promises")).readFile((await import("path")).resolve(`${(await import("path")).dirname((await import("url")).fileURLToPath(import.meta.url))}/../../Target/Workflow/Cloudflare.yml`),"utf-8")).toString()])}]);export{e as default};
