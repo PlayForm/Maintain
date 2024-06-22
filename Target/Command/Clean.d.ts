5d549c2cff1413076c6e9f6e0da73c270c2bdfe5
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-22 17:10:48 +0300

    squash!

--- Target/Command/Clean.d.ts
+++ Target/Command/Clean.d.ts
@@ -4,7 +4,7 @@
  */
 declare const _default: (Repositories?: string[]) => Promise<void>;
 export default _default;
-export declare const Request: (Where: string, With?: {}, Type?: string) => Promise<any>;
+export declare const Request: (Where: string, With?: {}, Type?: string) => Promise<import("@octokit/types").OctokitResponse<any, number> | any>;
 export declare const User: string;
 export declare const All: {
     Organizations: {
--- Target/Command/Dispatch.d.ts
+++ Target/Command/Dispatch.d.ts
@@ -4,4 +4,4 @@
  */
 declare const _default: (Repository?: string[] | Set<string>) => Promise<void>;
 export default _default;
-export declare const Request: (Where: string, With?: {}, Type?: string) => Promise<any>;
+export declare const Request: (Where: string, With?: {}, Type?: string) => Promise<import("@octokit/types").OctokitResponse<any, number> | any>;
