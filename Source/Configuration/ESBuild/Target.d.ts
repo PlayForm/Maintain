a0c139f8912bd166a0cb36b81f597c3c2bbb26cd
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-05-26 19:16:36 +0300

    chore: Refactor BuildOptions in TypeScript definitions and update package.json dependencies
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Configuration/ESBuild/Target.d.ts
+++ Source/Configuration/ESBuild/Target.d.ts
@@ -1,28 +1,2 @@
-declare const _default: {
-    color: true;
-    format: "esm";
-    logLevel: "debug";
-    metafile: true;
-    minify: true;
-    outdir: string;
-    platform: "node";
-    target: string;
-    tsconfig: string;
-    write: true;
-    legalComments: "none";
-    bundle: false;
-    assetNames: string;
-    sourcemap: false;
-    drop: "debugger"[];
-    ignoreAnnotations: true;
-    keepNames: false;
-    plugins: never[];
-    define: {
-        "process.env.VERSION_CLOUDFLARE_WRANGLER_ACTION": string;
-        "process.env.VERSION_ACTIONS_SETUP_NODE": string;
-        "process.env.VERSION_ACTIONS_UPLOAD_ARTIFACT": string;
-        "process.env.VERSION_ACTIONS_CACHE": string;
-        "process.env.VERSION_ACTIONS_RS_CARGO": string;
-    };
-};
+declare const _default: BuildOptions;
 export default _default;
